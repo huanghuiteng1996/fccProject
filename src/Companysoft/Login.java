765e1cf9836eed8cb2a12f54d5a3717ec5b7f825
黄辉腾 <48154382+huanghuiteng1996@users.noreply.github.com>
2019-04-10 23:17:30 +0800

    Add files via upload

---
+++ src/Companysoft/Login.java
@@ -0,0 +1,19 @@
+package mingrisoft;
+import java.sql.Connection;                          
+import java.sql.SQLException;				        
+public class Login {
+
+	DBConnection DBConn = new DBConnection();     	
+	Function Fun = new Function();                 	
+	
+	public boolean LoginCheck(String user, String pwd) {
+		try {
+			Connection Conn = DBConn.getConn();  	
+			boolean OK = true;                     	
+			OK = Fun.CheckLogin(Conn, user, pwd);	
+			return OK;					        	
+		} catch (SQLException e) {
+			return false;  							
+		}
+	}
+}

8db587bbc18350a8ec9871d4cdc6071e81c18b88
黄辉腾 <48154382+huanghuiteng1996@users.noreply.github.com>
2019-04-12 23:21:09 +0800

    Add files via upload

--- src/Companysoft/News.java
+++ src/Companysoft/News.java
@@ -1,45 +1,55 @@
 package mingrisoft;
 
-import java.sql.Connection;								
-import java.sql.ResultSet;								
-import java.sql.Statement;								
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class News {
 
-	DBConnection DBConn = new DBConnection();          		
-	Function Fun = new Function();				    	
+	DBConnection DBConn = new DBConnection();
+	Function Fun = new Function();
 
 	public String ListNews(String toPage, String pageNum) {
 		try {
-			Connection Conn = DBConn.getConn();			
-			Statement stmt = Conn.createStatement();		
-			ResultSet rs = null;					
-			StringBuffer resultData = new StringBuffer();		
-			String sSql = "select * from News order by NewsID desc";		
-			rs = stmt.executeQuery(sSql);				
-			resultData=Fun.ListNews(resultData,rs,toPage,pageNum);  
-			rs.close();						
-			stmt.close();						
-			Conn.close();						
-			return resultData.toString();				
+			Connection Conn = DBConn.getConn();
+			Statement stmt = Conn.createStatement();
+			ResultSet rs = null;
+			StringBuffer resultData = new StringBuffer();
+			String sSql = "select * from News order by NewsID desc";
+			rs = stmt.executeQuery(sSql);
+			resultData = Fun.ListNews(resultData, rs, toPage, pageNum);
+			rs.close();
+			stmt.close();
+			Conn.close();
+			return resultData.toString();
 		} catch (Exception e) {
-			return "No";						
+			return "No";
 		}
 	}
-	public String AddNews(String [] newsData)
-	 {			
-		try
-			{
-			Connection Conn = DBConn.getConn();					
-			Statement stmt = Conn.createStatement();				
-			ResultSet rs = null;								
-			String sSql = "select * from News order by NewsID desc";		
-		rs = stmt.executeQuery(sSql); 					      		
-		String result=Fun.AddNews(Conn,stmt,rs,newsData);		  		
-		return result;									
-		}catch(Exception e){
-		return "ÃÌº” ß∞‹";							 	
+
+	public String AddNews(String[] newsData) {
+		try {
+			Connection Conn = DBConn.getConn();
+			Statement stmt = Conn.createStatement();
+			ResultSet rs = null;
+			String sSql = "select * from News order by NewsID desc";
+			rs = stmt.executeQuery(sSql);
+			String result = Fun.AddNews(Conn, stmt, rs, newsData);
+			return result;
+		} catch (Exception e) {
+			return "ÃÌº” ß∞‹";
+		}
 	}
-}
+
+	public boolean DelNews(String newsId) {
+		try {
+			Connection Conn = DBConn.getConn();
+			Statement stmt = Conn.createStatement();
+			int NewsID = Fun.StrToInt(newsId);
+			return Fun.DelNews(Conn, stmt, NewsID);
+		} catch (Exception e) {
+			return false;
+		}
+	}
 
 }

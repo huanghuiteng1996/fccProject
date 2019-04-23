bb07d348bdc67d84050382ff7e5b632d6ed3c83b
黄辉腾 <48154382+huanghuiteng1996@users.noreply.github.com>
2019-04-23 23:20:48 +0800

    Add files via upload

--- src/Companysoft/News.java
+++ src/Companysoft/News.java
@@ -61,25 +61,53 @@ public class News {
 			return "数据库连接失败!";
 		}
 	}
-	public String ListNewsFront(String toPage,String pageNum)
-    {			
-		try
-    	{
-    		Connection Conn = DBConn.getConn();						
-	    	Statement stmt = Conn.createStatement();					
-	    	ResultSet rs = null;								
-    		StringBuffer resultData = new StringBuffer();					
-    		String sSql = "select * from News order by NewsID desc";			
-	    	rs = stmt.executeQuery(sSql);							
-	    	resultData=Fun.ListNewsFront(resultData,rs,toPage,pageNum);		
-		rs.close();									
-    		stmt.close();									
-    		Conn.close();									
-    		return resultData.toString();							
-    	}catch(Exception e)
-        {
-            return "No";									
-        }
-    }
+
+	public String ListNewsFront(String toPage, String pageNum) {
+		try {
+			Connection Conn = DBConn.getConn();
+			Statement stmt = Conn.createStatement();
+			ResultSet rs = null;
+			StringBuffer resultData = new StringBuffer();
+			String sSql = "select * from News order by NewsID desc";
+			rs = stmt.executeQuery(sSql);
+			resultData = Fun.ListNewsFront(resultData, rs, toPage, pageNum);
+			rs.close();
+			stmt.close();
+			Conn.close();
+			return resultData.toString();
+		} catch (Exception e) {
+			return "No";
+		}
+	}
+
+	public String FrontNewsDetail(String id) {
+		try {
+			Connection Conn = DBConn.getConn();
+			Statement stmt = Conn.createStatement();
+			ResultSet rs = null;
+			int NewsID = Fun.StrToInt(id);
+			if (NewsID == 0)
+				return "No";
+			else {
+				try {
+
+					String sql = "select * from News where NewsID=" + NewsID;
+					rs = stmt.executeQuery(sql);
+					StringBuffer sb = new StringBuffer();
+					Fun.FrontNewsDetail(sb, rs);
+					rs.close();
+					stmt.close();
+					Conn.close();
+					return sb.toString();
+				} catch (Exception e) {
+					Conn.rollback();
+					Conn.close();
+					return "No";
+				}
+			}
+		} catch (Exception e) {
+			return "No";
+		}
+	}
 
 }
